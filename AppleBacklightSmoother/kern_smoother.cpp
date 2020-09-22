ad0a005f63bc5b3d0a924b68bf3fee1b25fb1213
Le Bao Hiep <baohiep2013@gmail.com>
2020-09-23 05:50:50 +0700

    Cleanup

--- AppleBacklightSmoother/kern_smoother.cpp
+++ AppleBacklightSmoother/kern_smoother.cpp
@@ -12,80 +12,11 @@
 
 #include "kern_smoother.hpp"
 
-static const char *pathIntelSNBFb[]   { "/System/Library/Extensions/AppleIntelSNBGraphicsFB.kext/Contents/MacOS/AppleIntelSNBGraphicsFB" };
-static const char *pathIntelCapriFb[] { "/System/Library/Extensions/AppleIntelFramebufferCapri.kext/Contents/MacOS/AppleIntelFramebufferCapri" };
-static const char *pathIntelAzulFb[]  { "/System/Library/Extensions/AppleIntelFramebufferAzul.kext/Contents/MacOS/AppleIntelFramebufferAzul" };
-static const char *pathIntelBDWFb[]   { "/System/Library/Extensions/AppleIntelBDWGraphicsFramebuffer.kext/Contents/MacOS/AppleIntelBDWGraphicsFramebuffer" };
-static const char *pathIntelSKLFb[]   { "/System/Library/Extensions/AppleIntelSKLGraphicsFramebuffer.kext/Contents/MacOS/AppleIntelSKLGraphicsFramebuffer" };
-static const char *pathIntelKBLFb[]   { "/System/Library/Extensions/AppleIntelKBLGraphicsFramebuffer.kext/Contents/MacOS/AppleIntelKBLGraphicsFramebuffer" };
-static const char *pathIntelCFLFb[]   { "/System/Library/Extensions/AppleIntelCFLGraphicsFramebuffer.kext/Contents/MacOS/AppleIntelCFLGraphicsFramebuffer" };
-static const char *pathIntelCNLFb[]   { "/System/Library/Extensions/AppleIntelCNLGraphicsFramebuffer.kext/Contents/MacOS/AppleIntelCNLGraphicsFramebuffer" };
-static const char *pathIntelICLLPFb[] { "/System/Library/Extensions/AppleIntelICLLPGraphicsFramebuffer.kext/Contents/MacOS/AppleIntelICLLPGraphicsFramebuffer" };
-static const char *pathIntelICLHPFb[] { "/System/Library/Extensions/AppleIntelICLHPGraphicsFramebuffer.kext/Contents/MacOS/AppleIntelICLHPGraphicsFramebuffer" };
-
-static KernelPatcher::KextInfo kextIntelSNBFb   { "com.apple.driver.AppleIntelSNBGraphicsFB", pathIntelSNBFb, arrsize(pathIntelSNBFb), {}, {}, KernelPatcher::KextInfo::Unloaded };
-static KernelPatcher::KextInfo kextIntelCapriFb { "com.apple.driver.AppleIntelFramebufferCapri", pathIntelCapriFb, arrsize(pathIntelCapriFb), {}, {}, KernelPatcher::KextInfo::Unloaded };
-static KernelPatcher::KextInfo kextIntelAzulFb  { "com.apple.driver.AppleIntelFramebufferAzul", pathIntelAzulFb, arrsize(pathIntelAzulFb), {}, {}, KernelPatcher::KextInfo::Unloaded };
-static KernelPatcher::KextInfo kextIntelBDWFb   { "com.apple.driver.AppleIntelBDWGraphicsFramebuffer", pathIntelBDWFb, arrsize(pathIntelBDWFb), {}, {}, KernelPatcher::KextInfo::Unloaded };
-static KernelPatcher::KextInfo kextIntelSKLFb   { "com.apple.driver.AppleIntelSKLGraphicsFramebuffer", pathIntelSKLFb, arrsize(pathIntelSKLFb), {}, {}, KernelPatcher::KextInfo::Unloaded };
-static KernelPatcher::KextInfo kextIntelKBLFb   { "com.apple.driver.AppleIntelKBLGraphicsFramebuffer", pathIntelKBLFb, arrsize(pathIntelKBLFb), {}, {}, KernelPatcher::KextInfo::Unloaded };
-static KernelPatcher::KextInfo kextIntelCFLFb   { "com.apple.driver.AppleIntelCFLGraphicsFramebuffer", pathIntelCFLFb, arrsize(pathIntelCFLFb), {}, {}, KernelPatcher::KextInfo::Unloaded };
-static KernelPatcher::KextInfo kextIntelCNLFb   { "com.apple.driver.AppleIntelCNLGraphicsFramebuffer", pathIntelCNLFb, arrsize(pathIntelCNLFb), {}, {}, KernelPatcher::KextInfo::Unloaded };
-static KernelPatcher::KextInfo kextIntelICLLPFb { "com.apple.driver.AppleIntelICLLPGraphicsFramebuffer", pathIntelICLLPFb, arrsize(pathIntelICLLPFb), {}, {}, KernelPatcher::KextInfo::Unloaded };
-static KernelPatcher::KextInfo kextIntelICLHPFb { "com.apple.driver.AppleIntelICLHPGraphicsFramebuffer", pathIntelICLHPFb, arrsize(pathIntelICLHPFb), {}, {}, KernelPatcher::KextInfo::Unloaded };
-
-static AppleBacklightSmootherController *ADDPR(callbackSmoother);
-static IOCommandGate* ADDPR(cmdGate);
-
-static KernelPatcher::KextInfo *ADDPR(currentFramebuffer);
-static KernelPatcher::KextInfo *ADDPR(currentFramebufferOpt);
-
-static uint32_t (*ADDPR(orgHwSetBacklight))(void *, uint32_t);
-static bool ADDPR(backlightValueAssigned);
-static uint32_t ADDPR(backlightValue);
-
-#define super IOService
-OSDefineMetaClassAndStructors(AppleBacklightSmootherController, IOService)
-
-bool AppleBacklightSmootherController::start(IOService *provider) {
-	if (ADDPR(callbackSmoother)) {
-		return false;
-	}
-
-	if (!super::start(provider)) {
-		SYSLOG("smoother", "failed to start super");
-		return false;
-	}
-
-	ADDPR(callbackSmoother) = this;
-
-	IOWorkLoop* workLoop = getWorkLoop();
-	if (!workLoop) {
-		return false;
-	}
-
-	ADDPR(cmdGate) = IOCommandGate::commandGate(this);
-	if (ADDPR(cmdGate)) {
-		workLoop->addEventSource(ADDPR(cmdGate));
-	}
-
-	return true;
-}
-
-EXPORT IOReturn ADDPR(wrapHwSetBacklight)(void *that, uint32_t backlight) {
-	if (ADDPR(cmdGate)) {
-		ADDPR(cmdGate)->runAction(OSMemberFunctionCast(IOCommandGate::Action, ADDPR(callbackSmoother), &AppleBacklightSmootherController::wrapHwSetBacklightGated), that, (void *)&backlight);
-	} else {
-		ADDPR(orgHwSetBacklight)(that, backlight);
-	}
-	return kIOReturnSuccess;
-}
-
-IOReturn AppleBacklightSmootherController::wrapHwSetBacklightGated(void *that, uint32_t *backlight) {
+IOReturn AppleBacklightSmootherDummy::wrapHwSetBacklightGated(void *that, uint32_t *backlight) {
 	DBGLOG("smoother", "wrapHwSetBacklight called: backlight 0x%x", *backlight);
 
-	if (ADDPR(backlightValueAssigned)) {
-		uint32_t curBacklight = ADDPR(backlightValue);
+	if (SmootherData::backlightValueAssigned) {
+		uint32_t curBacklight = SmootherData::backlightValue;
 		uint32_t newBacklight = *backlight;
 		uint32_t diff = (newBacklight > curBacklight) ? (newBacklight - curBacklight) : (curBacklight - newBacklight);
 		if (diff > 0x10) {
@@ -94,31 +25,53 @@ IOReturn AppleBacklightSmootherController::wrapHwSetBacklightGated(void *that, u
 			if (newBacklight > curBacklight) {
 				while ((curBacklight += delta) < newBacklight) {
 					DBGLOG("smoother", "wrapHwSetBacklight set backlight 0x%x delta %d", curBacklight, delta);
-					ADDPR(orgHwSetBacklight)(that, curBacklight);
+					SmootherData::orgHwSetBacklight(that, curBacklight);
 					IOSleep(10); // Wait 5 miliseconds
 				}
 			} else {
 				while (curBacklight > delta && (curBacklight -= delta) > newBacklight) {
 					DBGLOG("smoother", "wrapHwSetBacklight set backlight 0x%x step %d", curBacklight, delta);
-					ADDPR(orgHwSetBacklight)(that, curBacklight);
+					SmootherData::orgHwSetBacklight(that, curBacklight);
 					IOSleep(10); // Wait 5 miliseconds
 				}
 			}
 		}
 	}
 
-	ADDPR(backlightValue) = *backlight;
-	ADDPR(backlightValueAssigned) = true;
-	ADDPR(orgHwSetBacklight)(that, *backlight);
+	SmootherData::backlightValue = *backlight;
+	SmootherData::backlightValueAssigned = true;
+	SmootherData::orgHwSetBacklight(that, *backlight);
 
 	return kIOReturnSuccess;
 }
 
-void ADDPR(processKext)(KernelPatcher &patcher, size_t index, mach_vm_address_t address, size_t size) {
-	if ((ADDPR(currentFramebuffer) && ADDPR(currentFramebuffer)->loadIndex == index) ||
-		(ADDPR(currentFramebufferOpt) && ADDPR(currentFramebufferOpt)->loadIndex == index)) {
+IOReturn SmootherData::wrapHwSetBacklight(void *that, uint32_t backlight) {
+	if (!triedCmdGate && !SmootherData::cmdGate && ADDPR(selfInstance)) {
+		triedCmdGate = true;
+
+		IOWorkLoop* workLoop = ADDPR(selfInstance)->getWorkLoop();
+		if (workLoop) {
+			SmootherData::cmdGate = IOCommandGate::commandGate(ADDPR(selfInstance));
+			if (SmootherData::cmdGate) {
+				workLoop->addEventSource(SmootherData::cmdGate);
+			}
+		}
+	}
+
+	if (SmootherData::cmdGate) {
+		SmootherData::cmdGate->runAction(OSMemberFunctionCast(IOCommandGate::Action, nullptr, &AppleBacklightSmootherDummy::wrapHwSetBacklightGated), that, (void *)&backlight);
+	} else {
+		SmootherData::orgHwSetBacklight(that, backlight);
+	}
+
+	return kIOReturnSuccess;
+}
+
+void SmootherData::processKext(KernelPatcher &patcher, size_t index, mach_vm_address_t address, size_t size) {
+	if ((SmootherData::currentFramebuffer && SmootherData::currentFramebuffer->loadIndex == index) ||
+		(SmootherData::currentFramebufferOpt && SmootherData::currentFramebufferOpt->loadIndex == index)) {
 		// Find actual framebuffer used
-		auto realFramebuffer = (ADDPR(currentFramebuffer) && ADDPR(currentFramebuffer)->loadIndex == index) ? ADDPR(currentFramebuffer) : ADDPR(currentFramebufferOpt);
+		auto realFramebuffer = (SmootherData::currentFramebuffer && SmootherData::currentFramebuffer->loadIndex == index) ? SmootherData::currentFramebuffer : SmootherData::currentFramebufferOpt;
 
 		// Find hwSetBacklight symbol
 		auto hwSetBacklightSym = "";
@@ -143,8 +96,8 @@ void ADDPR(processKext)(KernelPatcher &patcher, size_t index, mach_vm_address_t 
 		}
 
 		// Route hwSetBacklight to wrapHwSetBacklight
-		ADDPR(orgHwSetBacklight) = reinterpret_cast<decltype(ADDPR(orgHwSetBacklight))>(patcher.routeFunction(reinterpret_cast<mach_vm_address_t>(solvedSymbol), reinterpret_cast<mach_vm_address_t>(ADDPR(wrapHwSetBacklight)), true));
-		if (!ADDPR(orgHwSetBacklight)) {
+		SmootherData::orgHwSetBacklight = reinterpret_cast<decltype(SmootherData::orgHwSetBacklight)>(patcher.routeFunction(reinterpret_cast<mach_vm_address_t>(solvedSymbol), reinterpret_cast<mach_vm_address_t>(SmootherData::wrapHwSetBacklight), true));
+		if (!SmootherData::orgHwSetBacklight) {
 			SYSLOG("smoother", "Failed to route hwSetBacklight");
 			patcher.clearError();
 			return;
@@ -154,14 +107,14 @@ void ADDPR(processKext)(KernelPatcher &patcher, size_t index, mach_vm_address_t 
 	}
 }
 
-void ADDPR(init_plugin)() {
-	ADDPR(callbackSmoother) = nullptr;
-	ADDPR(cmdGate) = nullptr;
-	ADDPR(orgHwSetBacklight) = nullptr;
-	ADDPR(currentFramebuffer) = ADDPR(currentFramebufferOpt) = nullptr;
-
-	ADDPR(backlightValueAssigned) = false;
-	ADDPR(backlightValue) = 0;
+void SmootherData::init_plugin() {
+	SmootherData::cmdGate = nullptr;
+	SmootherData::triedCmdGate = false;
+	SmootherData::currentFramebuffer = nullptr;
+	SmootherData::currentFramebufferOpt = nullptr;
+	SmootherData::orgHwSetBacklight = nullptr;
+	SmootherData::backlightValueAssigned = false;
+	SmootherData::backlightValue = 0;
 
 	auto &bdi = BaseDeviceInfo::get();
 	auto generation = bdi.cpuGeneration;
@@ -174,52 +127,52 @@ void ADDPR(init_plugin)() {
 			// Do not warn about legacy processors
 			break;
 		case CPUInfo::CpuGeneration::SandyBridge:
-			ADDPR(currentFramebuffer) = &kextIntelSNBFb;
+			SmootherData::currentFramebuffer = &kextIntelSNBFb;
 			break;
 		case CPUInfo::CpuGeneration::IvyBridge:
-			ADDPR(currentFramebuffer) = &kextIntelCapriFb;
+			SmootherData::currentFramebuffer = &kextIntelCapriFb;
 			break;
 		case CPUInfo::CpuGeneration::Haswell:
-			ADDPR(currentFramebuffer) = &kextIntelAzulFb;
+			SmootherData::currentFramebuffer = &kextIntelAzulFb;
 			break;
 		case CPUInfo::CpuGeneration::Broadwell:
-			ADDPR(currentFramebuffer) = &kextIntelBDWFb;
+			SmootherData::currentFramebuffer = &kextIntelBDWFb;
 			break;
 		case CPUInfo::CpuGeneration::Skylake:
-			ADDPR(currentFramebuffer) = &kextIntelSKLFb;
+			SmootherData::currentFramebuffer = &kextIntelSKLFb;
 			break;
 		case CPUInfo::CpuGeneration::KabyLake:
-			ADDPR(currentFramebuffer) = &kextIntelKBLFb;
+			SmootherData::currentFramebuffer = &kextIntelKBLFb;
 			break;
 		case CPUInfo::CpuGeneration::CoffeeLake:
-			ADDPR(currentFramebuffer) = &kextIntelCFLFb;
-			ADDPR(currentFramebufferOpt) = &kextIntelKBLFb;
+			SmootherData::currentFramebuffer = &kextIntelCFLFb;
+			SmootherData::currentFramebufferOpt = &kextIntelKBLFb;
 			break;
 		case CPUInfo::CpuGeneration::CannonLake:
-			ADDPR(currentFramebuffer) = &kextIntelCNLFb;
+			SmootherData::currentFramebuffer = &kextIntelCNLFb;
 			break;
 		case CPUInfo::CpuGeneration::IceLake:
-			ADDPR(currentFramebuffer) = &kextIntelICLLPFb;
-			ADDPR(currentFramebufferOpt) = &kextIntelICLHPFb;
+			SmootherData::currentFramebuffer = &kextIntelICLLPFb;
+			SmootherData::currentFramebufferOpt = &kextIntelICLHPFb;
 			break;
 		case CPUInfo::CpuGeneration::CometLake:
-			ADDPR(currentFramebuffer) = &kextIntelCFLFb;
-			ADDPR(currentFramebufferOpt) = &kextIntelKBLFb;
+			SmootherData::currentFramebuffer = &kextIntelCFLFb;
+			SmootherData::currentFramebufferOpt = &kextIntelKBLFb;
 			break;
 		default:
 			SYSLOG("smoother", "found an unsupported processor 0x%X:0x%X, please report this!", family, model);
 			break;
 	}
 
-	if (ADDPR(currentFramebuffer))
-		lilu.onKextLoadForce(ADDPR(currentFramebuffer));
+	if (SmootherData::currentFramebuffer)
+		lilu.onKextLoadForce(SmootherData::currentFramebuffer);
 
-	if (ADDPR(currentFramebufferOpt))
-		lilu.onKextLoadForce(ADDPR(currentFramebufferOpt));
+	if (SmootherData::currentFramebufferOpt)
+		lilu.onKextLoadForce(SmootherData::currentFramebufferOpt);
 
 	lilu.onKextLoadForce(nullptr, 0,
 	[](void *user, KernelPatcher &patcher, size_t index, mach_vm_address_t address, size_t size) {
-		ADDPR(processKext)(patcher, index, address, size);
+		SmootherData::processKext(patcher, index, address, size);
 	}, nullptr);
 }
 
@@ -248,6 +201,6 @@ PluginConfiguration ADDPR(config) {
 	KernelVersion::MountainLion,
 	KernelVersion::BigSur,
 	[]() {
-		ADDPR(init_plugin)();
+		SmootherData::init_plugin();
 	}
 };
--- AppleBacklightSmoother/kern_smoother.hpp
+++ AppleBacklightSmoother/kern_smoother.hpp
@@ -8,15 +8,51 @@
 #ifndef kern_smoother_hpp
 #define kern_smoother_hpp
 
-class AppleBacklightSmootherController : public IOService {
-	OSDeclareDefaultStructors(AppleBacklightSmootherController)
+static const char *pathIntelSNBFb[]   { "/System/Library/Extensions/AppleIntelSNBGraphicsFB.kext/Contents/MacOS/AppleIntelSNBGraphicsFB" };
+static const char *pathIntelCapriFb[] { "/System/Library/Extensions/AppleIntelFramebufferCapri.kext/Contents/MacOS/AppleIntelFramebufferCapri" };
+static const char *pathIntelAzulFb[]  { "/System/Library/Extensions/AppleIntelFramebufferAzul.kext/Contents/MacOS/AppleIntelFramebufferAzul" };
+static const char *pathIntelBDWFb[]   { "/System/Library/Extensions/AppleIntelBDWGraphicsFramebuffer.kext/Contents/MacOS/AppleIntelBDWGraphicsFramebuffer" };
+static const char *pathIntelSKLFb[]   { "/System/Library/Extensions/AppleIntelSKLGraphicsFramebuffer.kext/Contents/MacOS/AppleIntelSKLGraphicsFramebuffer" };
+static const char *pathIntelKBLFb[]   { "/System/Library/Extensions/AppleIntelKBLGraphicsFramebuffer.kext/Contents/MacOS/AppleIntelKBLGraphicsFramebuffer" };
+static const char *pathIntelCFLFb[]   { "/System/Library/Extensions/AppleIntelCFLGraphicsFramebuffer.kext/Contents/MacOS/AppleIntelCFLGraphicsFramebuffer" };
+static const char *pathIntelCNLFb[]   { "/System/Library/Extensions/AppleIntelCNLGraphicsFramebuffer.kext/Contents/MacOS/AppleIntelCNLGraphicsFramebuffer" };
+static const char *pathIntelICLLPFb[] { "/System/Library/Extensions/AppleIntelICLLPGraphicsFramebuffer.kext/Contents/MacOS/AppleIntelICLLPGraphicsFramebuffer" };
+static const char *pathIntelICLHPFb[] { "/System/Library/Extensions/AppleIntelICLHPGraphicsFramebuffer.kext/Contents/MacOS/AppleIntelICLHPGraphicsFramebuffer" };
+
+static KernelPatcher::KextInfo kextIntelSNBFb   { "com.apple.driver.AppleIntelSNBGraphicsFB", pathIntelSNBFb, arrsize(pathIntelSNBFb), {}, {}, KernelPatcher::KextInfo::Unloaded };
+static KernelPatcher::KextInfo kextIntelCapriFb { "com.apple.driver.AppleIntelFramebufferCapri", pathIntelCapriFb, arrsize(pathIntelCapriFb), {}, {}, KernelPatcher::KextInfo::Unloaded };
+static KernelPatcher::KextInfo kextIntelAzulFb  { "com.apple.driver.AppleIntelFramebufferAzul", pathIntelAzulFb, arrsize(pathIntelAzulFb), {}, {}, KernelPatcher::KextInfo::Unloaded };
+static KernelPatcher::KextInfo kextIntelBDWFb   { "com.apple.driver.AppleIntelBDWGraphicsFramebuffer", pathIntelBDWFb, arrsize(pathIntelBDWFb), {}, {}, KernelPatcher::KextInfo::Unloaded };
+static KernelPatcher::KextInfo kextIntelSKLFb   { "com.apple.driver.AppleIntelSKLGraphicsFramebuffer", pathIntelSKLFb, arrsize(pathIntelSKLFb), {}, {}, KernelPatcher::KextInfo::Unloaded };
+static KernelPatcher::KextInfo kextIntelKBLFb   { "com.apple.driver.AppleIntelKBLGraphicsFramebuffer", pathIntelKBLFb, arrsize(pathIntelKBLFb), {}, {}, KernelPatcher::KextInfo::Unloaded };
+static KernelPatcher::KextInfo kextIntelCFLFb   { "com.apple.driver.AppleIntelCFLGraphicsFramebuffer", pathIntelCFLFb, arrsize(pathIntelCFLFb), {}, {}, KernelPatcher::KextInfo::Unloaded };
+static KernelPatcher::KextInfo kextIntelCNLFb   { "com.apple.driver.AppleIntelCNLGraphicsFramebuffer", pathIntelCNLFb, arrsize(pathIntelCNLFb), {}, {}, KernelPatcher::KextInfo::Unloaded };
+static KernelPatcher::KextInfo kextIntelICLLPFb { "com.apple.driver.AppleIntelICLLPGraphicsFramebuffer", pathIntelICLLPFb, arrsize(pathIntelICLLPFb), {}, {}, KernelPatcher::KextInfo::Unloaded };
+static KernelPatcher::KextInfo kextIntelICLHPFb { "com.apple.driver.AppleIntelICLHPGraphicsFramebuffer", pathIntelICLHPFb, arrsize(pathIntelICLHPFb), {}, {}, KernelPatcher::KextInfo::Unloaded };
+
+namespace SmootherData {
+	static IOCommandGate* cmdGate;
+	static bool triedCmdGate;
+
+	static KernelPatcher::KextInfo *currentFramebuffer;
+	static KernelPatcher::KextInfo *currentFramebufferOpt;
+
+	static uint32_t (*orgHwSetBacklight)(void *, uint32_t);
+
+	static bool backlightValueAssigned;
+	static uint32_t backlightValue;
+
+	static IOReturn wrapHwSetBacklight(void *that, uint32_t backlight);
+
+	static void processKext(KernelPatcher &patcher, size_t index, mach_vm_address_t address, size_t size);
+
+	static void init_plugin();
+}
+
+class AppleBacklightSmootherDummy : public IOService {
+	OSDeclareDefaultStructors(AppleBacklightSmootherDummy)
 
 public:
-	bool start(IOService *provider) override;
-
-	/**
-	 *  AppleIntelFramebufferController::hwSetBacklight gated wrapper to smooth backlight transition
-	 */
 	IOReturn wrapHwSetBacklightGated(void *that, uint32_t *backlight);
 };
 
